bfa90650f105ada3e55e24dd3704684a32e42b6b
Connor Reddington <99690237+CWRedd@users.noreply.github.com>
2026-09-09 10:46:12 -0400

    panelsGamepad added to siddhant teleop

--- TeamCode/src/main/java/ClawTuner.java
+++ TeamCode/src/main/java/ClawTuner.java
@@ -1,24 +1,41 @@
+import com.bylazar.gamepad.PanelsGamepad;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.Gamepad;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.Range;
 
+/**
+ * Run this OFF the robot, or with nothing in the claw, to find the real
+ * open/closed servo positions. Step in small increments -- if the servo
+ * buzzes or gets warm you have hit the mechanical stop; back off immediately.
+ *
+ * Put the numbers you find into Claw.OPEN and Claw.CLOSED.
+ */
 @TeleOp(name = "Claw Tuner", group = "tuning")
 public class ClawTuner extends OpMode {
     private Servo claw;
     private double pos = 0.5;
     private boolean lastUp, lastDown;
+
     @Override
     public void init() {
         claw = hardwareMap.get(Servo.class, "claw");
     }
+    //0.1 extended, 0.67 retracted
+
     @Override
     public void loop() {
         // one press = one 0.01 step, so you creep up on the limits
-        if (gamepad1.right_bumper && !lastUp)   pos += 0.01;
-        if (gamepad1.left_bumper  && !lastDown) pos -= 0.01;
-        lastUp   = gamepad1.right_bumper;
-        lastDown = gamepad1.left_bumper;
+
+        Gamepad g1 = PanelsGamepad.INSTANCE
+                .getFirstManager()
+                .asCombinedFTCGamepad(gamepad1);
+
+        if (g1.right_bumper )   pos += 0.01;
+        if (g1.left_bumper  ) pos -= 0.01;
+//        lastUp   = g1.right_bumper;
+//        lastDown = g1.left_bumper;
 
         pos = Range.clip(pos, 0.0, 1.0);
         claw.setPosition(pos);
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumTeleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumTeleop.java
@@ -1,6 +1,8 @@
 package org.firstinspires.ftc.teamcode;
+import com.bylazar.gamepad.PanelsGamepad;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.Gamepad;
 
 import org.firstinspires.ftc.teamcode.opmode.MecanumDrive;
 @TeleOp(name = "MecanumSH")
@@ -18,34 +20,54 @@ public class MecanumTeleop extends OpMode {
         claw.init(hardwareMap);
         arm = new Arm();
         arm.init(hardwareMap);
+
         telemetry.addLine("Pready!");
         telemetry.update();
     }
     @Override
     public void loop() {
-        if (gamepad1.dpad_down) {
+        Gamepad g1 = PanelsGamepad.INSTANCE
+                .getFirstManager()
+                .asCombinedFTCGamepad(gamepad1);
+
+        if (g1.dpadDownWasPressed()) {
             speedMultiply = 0.25;
-        } else if (gamepad1.dpad_left) {
+        } else if (g1.dpadLeftWasPressed()) {
             speedMultiply = 0.5;
-        } else if (gamepad1.dpad_right) {
+        } else if (g1.dpadRightWasPressed()) {
             speedMultiply = 0.75;
-        } else if (gamepad1.dpad_up) {
+        } else if (g1.dpadUpWasPressed()) {
             speedMultiply = 1.0;
         }
-    double forward = -1*gamepad1.left_stick_y*speedMultiply;
-    double strafe = gamepad1.left_stick_x*speedMultiply;
-    double rotate = gamepad1.right_stick_x*speedMultiply;
+    double forward = -1*g1.left_stick_y*speedMultiply;
+    double strafe = g1.left_stick_x*speedMultiply;
+    double rotate = g1.right_stick_x*speedMultiply;
 
     drive.drive(forward, strafe, rotate);
-        if (gamepad1.right_bumper)     claw.close();
-        else if (gamepad1.left_bumper) claw.open();
-        if (gamepad1.y)      arm.setTarget(Arm.HIGH);
-        else if (gamepad1.b) arm.setTarget(Arm.MIDD);
-        else if (gamepad1.x) arm.setTarget(Arm.MIDH);
-        else if (gamepad1.a) arm.setTarget(Arm.DOWN);
-        double jog = (gamepad1.right_trigger - gamepad1.left_trigger) * ARM_JOG_RATE;
-        if (Math.abs(jog) > 0.1) arm.adjustTarget(jog);
-        arm.update();
+
+    if (g1.rightBumperWasPressed()) {
+        claw.close();
+    }
+    else if (g1.leftBumperWasPressed()){
+        claw.open();}
+    if (g1.yWasPressed()){
+        arm.setTarget(Arm.HIGH);
+    }
+    else if (g1.bWasPressed()){
+        arm.setTarget(Arm.MIDD);
+    }
+    else if (g1.xWasPressed()) {
+        arm.setTarget(Arm.MIDH);
+    }
+    else if (g1.aWasPressed()) {
+        arm.setTarget(Arm.DOWN);
+    }
+    double jog = (g1.right_trigger - g1.left_trigger) * ARM_JOG_RATE;
+
+    if (Math.abs(jog) > 0.1) arm.adjustTarget(jog);
+
+    arm.update();
+
     telemetry.addData("speed", speedMultiply);
     telemetry.addData("claw", "%.3f", claw.getPosition());
     telemetry.addData("arm pos/target", "%d / %.0f", arm.getPosition(), arm.getTarget());
